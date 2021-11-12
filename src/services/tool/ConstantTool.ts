e9b4df71f64a2b334673f49df547f144058c5565
somaxa8 <silviofrancoxa8@gmail.com>
2021-11-12 15:11:58 -0500

    rename project name

--- src/services/tool/ConstantTool.ts
+++ src/services/tool/ConstantTool.ts
@@ -5,6 +5,6 @@ export default class ConstantTool {
     static readonly CLIENT_SECRET = process.env.VUE_APP_CLIENT_SECRET
     static readonly ENCRYPT_SESSION: boolean = process.env.VUE_APP_ENCRYPT_SESSION === "true"
 
-    static readonly PROJECT_NAME = "vue-template"
+    static readonly PROJECT_NAME = "Celmybell"
 
 }
